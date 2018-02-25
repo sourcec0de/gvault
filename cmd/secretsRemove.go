9baab97c3c9d044392f6a2e3756bf291095b277a
James Qualls <qualls.james@gmail.com>
2018-02-24 20:48:40 -0700

    added secret remove cmd

---
+++ cmd/secretsRemove.go
@@ -0,0 +1,35 @@
+// Copyright © 2018 James Qualls https://github.com/sourcec0de
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+)
+
+// secretsRemoveCmd represents the remove command
+var secretsRemoveCmd = &cobra.Command{
+	Use:   "remove",
+	Short: "delete a secret from the vault",
+	Long:  "gvault secrets remove SECRET_NAME",
+	Args:  cobra.MinimumNArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		secretsCmd.vault.RemoveSecret(args[0])
+		secretsCmd.vault.Save()
+	},
+}
+
+func init() {
+	secretsCmd.AddCommand(secretsRemoveCmd)
+}
